82ecedc73e191c54a2ef11427d52cf2eb93415e5
Benjamin Eckel <bhelx@simst.im>
2015-10-07 12:13:39 -0700

    move brace

--- lib/recurly/unique_coupon_list.php
+++ lib/recurly/unique_coupon_list.php
@@ -2,8 +2,7 @@
 
 class Recurly_UniqueCouponList extends Recurly_Pager
 {
-  public static function get($params = null, $client = null)
-  {
+  public static function get($params = null, $client = null) {
     $list = new Recurly_UniqueCouponList(Recurly_Client::PATH_UNIQUE_COUPONS, $client);
     $list->_loadFrom(Recurly_Client::PATH_UNIQUE_COUPONS, $params);
     return $list;
